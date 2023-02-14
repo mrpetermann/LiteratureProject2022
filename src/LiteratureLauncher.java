49e96b107b216fe02455f2e2e21868871cd49567
mpetermann <mpetermann@waukesha.k12.wi.us>
2023-02-14 12:10:51 -0600

    update v3

--- src/LiteratureLauncher.java
+++ src/LiteratureLauncher.java
@@ -3,63 +3,51 @@ import java.util.Scanner;
 //Controller
 public class LiteratureLauncher {
     public static void main(String[] a) {
+        Book bookA = getBook();
+        Book bookB = getBook();
+
+        //Output Book one
+        printBook(bookA);
+        printBook(bookB);
+
+        //Compare Book objects
+        System.out.println("Books are the same: " + bookA.equals(bookB));
+    }
+
+    /**
+     * Collect user input to build a book
+     * @return New book object with info from user
+     */
+    private static Book getBook() {
         //Variables
         Scanner in = new Scanner(System.in);
         String title, author, publisher, isbn;
-        Book bookA = null, bookB = null;
 
-        //Prompt Book one
+        //Prompt Book entry
         System.out.println("Enter title of first book:");
         title = in.nextLine();
         System.out.println("Enter author of first book:");
         author = in.nextLine();
         System.out.println("Enter publisher of the first book:");
         publisher = in.nextLine();
-        System.out.println("Enter ISBN of the first book:");
-        isbn = in.nextLine();
 
         //Validate ISBN
-        if(isbn.length() == 13)
-            bookA = new Book(title, author, publisher, isbn);
-        else
-            System.out.println("Invalid ISBN");
+        do {
+            System.out.println("Enter ISBN of the first book:");
+            isbn = in.nextLine();
+        } while (isbn.length() != 13);
 
-        //Prompt Book two
-        System.out.println("Enter title of second book:");
-        title = in.nextLine();
-        System.out.println("Enter author of second book:");
-        author = in.nextLine();
-        System.out.println("Enter publisher of the second book:");
-        publisher = in.nextLine();
-        System.out.println("Enter ISBN of the second book:");
-        isbn = in.nextLine();
+        return new Book(title, author, publisher, isbn);
+    }
 
-        //Validate ISBN
-        if(isbn.length() == 13)
-            bookB = new Book(title, author, publisher, isbn);
-        else
-            System.out.println("Invalid ISBN");
-
-        //Output Book one
-        if(bookA != null) {
-            System.out.println("Book One:");
-            System.out.println(bookA.getTitleByAuthor());
-            System.out.println("Publisher: " + bookA.getPublisher());
-            System.out.println("ISBN: " + bookA.getIsbn());
-            System.out.println();
-        }
-
-        //Output Book two
-        if(bookB != null) {
-            System.out.println("Book Two:");
-            System.out.println(bookB.getTitleByAuthor());
-            System.out.println("Publisher: " + bookB.getPublisher());
-            System.out.println("ISBN: " + bookB.getIsbn());
-            System.out.println();
-        }
-
-        //Compare Book objects
-        if(bookA != null && bookB != null)
-            System.out.println("Books are the same: " + bookA.equals(bookB));
+    /**
+     * Output data to the console
+     * @param book Book object to be printed
+     */
+    private static void printBook(Book book) {
+        System.out.println(book.getTitleByAuthor());
+        System.out.println("Publisher: " + book.getPublisher());
+        System.out.println("ISBN: " + book.getIsbn());
+        System.out.println();
     }
 }
